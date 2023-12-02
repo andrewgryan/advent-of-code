75a4cef5004328e6000ee7e14ac15e6808b198e1
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-12-02 18:22:15 +0000

    add a way to print a number

--- 2023/x86-64/day-1.asm
+++ 2023/x86-64/day-1.asm
@@ -1,5 +1,6 @@
 format ELF64 executable
 
+SYS_brk = 0x0c
 SYS_write = 1
 SYS_exit = 60
 STDOUT = 1
@@ -37,10 +38,70 @@ main:
         ; mul rdi              ; rax = rax * rdi
         ; add rax, rdi         ; rax = rax + rdi
 
+        mov rsi, 123456789
+        call print_register
+
         ; Exit system call
         mov rax, SYS_exit
-        mov rdi, rcx
+        mov rdi, rdx
         syscall
+
+
+; @param rsi - number to print
+print_register:
+        ; Estimate width in base 10
+        xor r8, r8           ; Set r8 to zero
+        mov rcx, 0x0a        ; Set divisor to 10
+        mov rax, rsi         ; Copy register
+.decimal_places:
+        inc r8
+        xor rdx, rdx
+        div rcx
+        cmp rax, 0
+        jnz .decimal_places
+        push r8              ; Save width on stack
+
+        ; Call brk to request memory address space
+        mov rax, SYS_brk
+        mov rdi, 0
+        syscall
+
+        ; Ask for a new breakpoint
+        lea rdi, [rax + r8]
+        mov rax, SYS_brk
+        syscall
+
+        ; Save previous breakpoint location
+        mov r11, rax         ; Copy string address
+        push r11             ; Save string address
+
+        ; Copy characters
+        mov rax, rsi         ; Copy register
+.copy_digits:
+        dec r8               ; Decrement digit counter
+        lea r9, [r11 + r8]   ; Next effective address
+
+        ; Divide number by 10
+        mov rcx, 0x0a        ; Set divisor to 10
+        xor rdx, rdx         ; Clear dividend register
+        div rcx              ; Perform unsigned division by 10
+
+        ; Write ASCII character
+        mov r10, rax         ; Save division
+        add rdx, 48          ; Convert remainder to ASCII
+        mov byte [r9], dl    ; Place digit in buffer
+
+        ; Loop condition
+        cmp r10, 0           ; Check division non-zero
+        jnz .copy_digits
+
+        ; Print system call
+        mov rax, SYS_write
+        mov rdi, STDOUT
+        pop rsi              ; String address
+        pop rdx              ; Retrieve string length
+        syscall
+        ret
 
 
 segment readable writable
