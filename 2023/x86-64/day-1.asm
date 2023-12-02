d8dc4925fd898c225c0a8d0cf6af9f574f472085
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-12-02 15:43:49 +0000

    boilerplate

---
+++ 2023/x86-64/day-1.asm
@@ -0,0 +1,26 @@
+format ELF64 executable
+
+SYS_write = 1
+SYS_exit = 60
+STDOUT = 1
+
+segment readable executable
+entry main
+main:
+        ; Print
+        mov rax, SYS_write
+        mov rdi, STDOUT
+        mov rsi, input
+        mov rdx, input_len
+        syscall
+
+        ; Exit system call
+        mov rax, SYS_exit
+        mov rdi, 0
+        syscall
+
+
+segment readable writable
+input file "input-1"
+input_len = $ - input
+
