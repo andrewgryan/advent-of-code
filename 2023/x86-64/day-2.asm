8c58d1927d76a03ab7d04c7f12475cc087e47605
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-12-03 18:24:04 +0000

    more boilerplate

--- 2023/x86-64/day-2.asm
+++ 2023/x86-64/day-2.asm
@@ -7,4 +7,10 @@ include "util.inc"
 segment readable executable
 entry main
 main:
+        print input, input_len
         exit 0
+
+
+segment readable writable
+input file "input-2"
+input_len = $ - input
