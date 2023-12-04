e8dd69bbeea5c999cca6903a593dfb4825f8d1a0
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-12-04 20:05:48 +0000

    parse game id

--- 2023/x86-64/day-2.asm
+++ 2023/x86-64/day-2.asm
@@ -15,16 +15,50 @@ main:
 
         print input, rdx
 
+        mov rsi, input
+        mov rdi, input_len
         call parse_game_id
         mov rdi, rax
         exit rdi
 
 
+; @param rsi - Address
+; @param rdi - Length
 parse_game_id:
-        mov rax, 1
+        mov rdx, game
+        mov rcx, game_len
+        call parse_prefix
+        cmp rax, 1
+        je .id
+        mov rax, -1
+        ret
+.id:
+        add rsi, rcx
+        call parse_digit
+        ret
+
+
+; @param rsi - Address
+parse_digit:
+        mov al, byte [rsi]
+
+        cmp al, 48
+        jl .fail
+
+        cmp al, 57
+        jg .fail
+
+        sub al, 48
+        ret
+
+.fail:
+        mov rax, -1
         ret
 
 
 segment readable writable
 input file "input-2"
 input_len = $ - input
+
+game db "Game "
+game_len = $ - game
