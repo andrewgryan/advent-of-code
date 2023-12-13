6c1814fc3e389771e656e5eadec4938e823d82b4
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-12-13 21:16:50 +0000

    calculate grid width and height

--- 2023/x86-64/day-3.asm
+++ 2023/x86-64/day-3.asm
@@ -4,7 +4,95 @@ format ELF64 executable
 include "util.inc"
 
 
+NEWLINE = 0x0a
+
+
 segment readable executable
 entry main
 main:
+        ; Load puzzle input
+        mov         rsi, input
+        mov         rdi, input_len
+        int3
+
+        ; Get grid width
+        call grid_width
+        int3
+
+        ; Get grid height
+        call grid_height
+        int3
+
         exit        0
+
+
+grid_width:
+        ;          Store string on stack
+        push       rdi
+        push       rsi
+        xor        rax, rax
+.next:
+        ;          Compare length
+        cmp        rdi, 0
+        je         .done
+
+        ;          Read character
+        mov        r8b, [rsi]
+        cmp        r8, NEWLINE
+        je         .done
+
+        ;          Advance pointer
+        inc        rsi
+        dec        rdi
+
+        ;          Add one
+        inc        rax
+        jmp        .next
+
+.done:
+        ;          Restore string
+        pop        rsi
+        pop        rdi
+        ret
+
+
+grid_height:
+        ;          Store string on stack
+        push       rdi
+        push       rsi
+        xor        rax, rax
+.next:
+        ;          Compare length
+        cmp        rdi, 0
+        je         .done
+
+        ;          Read character
+        mov        r8b, [rsi]
+        cmp        r8, NEWLINE
+        je         .add_one
+
+        ;          Advance pointer
+        inc        rsi
+        dec        rdi
+        jmp        .next
+
+.add_one:
+        ;          Advance pointer
+        inc        rsi
+        dec        rdi
+
+        ;          Add one
+        inc        rax
+        jmp        .next
+
+.done:
+        ;          Restore string
+        pop        rsi
+        pop        rdi
+        inc        rax
+        ret
+
+
+segment readable writable
+input file "input-3"
+input_len = $ - input
