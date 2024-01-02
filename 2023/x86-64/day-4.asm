38d28125c4f84e3dc5104e02a1eeab05bb2faaa5
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-01-02 22:29:25 +0000

    .

--- 2023/x86-64/day-4.asm
+++ 2023/x86-64/day-4.asm
@@ -7,4 +7,16 @@ include "util.inc"
 segment readable executable
 entry main
 main:
-        exit 0
+        mov        si, word [input + 10]
+        mov        [winners], si
+        exit       0
+
+
+segment readable writable
+
+; Data to hold scratch card information
+winners rw 10
+numbers rw 25
+
+input file "input-4"
+input_len = $ - input
