3b48b129920683a8a62dfd8167464e44f73feffc
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-01-03 09:21:26 +0000

    .

--- 2023/x86-64/day-4.asm
+++ 2023/x86-64/day-4.asm
@@ -7,16 +7,30 @@ include "util.inc"
 segment readable executable
 entry main
 main:
-        mov        si, word [input + 10]
-        mov        [winners], si
+        movzx      rsi, byte [input + 10]
+        movzx      rdi, byte [input + 11]
+        call       to_number
         exit       0
+
+
+; @param   {byte} sil - ASCII character
+; @param   {byte} dil - ASCII character
+;
+; @returns {byte} al - number between 0-99
+to_number:
+        sub        sil, '0'
+        sub        dil, '0'
+        movzx      rax, sil
+        imul       rax, 10
+        add        al, dil
+        ret
 
 
 segment readable writable
 
 ; Data to hold scratch card information
-winners rw 10
-numbers rw 25
+winners rb 10
+numbers rb 25
 
 input file "input-4"
 input_len = $ - input
