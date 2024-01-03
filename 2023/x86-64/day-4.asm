5c22d77013a096667c35e09ace6848c751d29ac7
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-01-03 14:42:48 +0000

    .

--- 2023/x86-64/day-4.asm
+++ 2023/x86-64/day-4.asm
@@ -7,16 +7,51 @@ include "util.inc"
 segment readable executable
 entry main
 main:
-        mov        rsi, input
-        mov        rdi, winners
-        call       read_winners
+        ;          Debug information
+        mov        rdi, upper
+        mov        rsi, lower
 
+        ;          100-bit mask encoding
+        mov        rcx, 72
+        call       encode
+        mov        rcx, 28
+        call       encode
+        mov        rcx, 41
+        call       encode
+        mov        rcx, 15
+        call       encode
+        mov        rcx, 98
+        call       encode
+        mov        rcx, 13
+        call       encode
         int3
-
         exit       0
 
-to_bitmask:
+encode:
+        cmp        rcx, 50
+        jb         .lower
+.upper:
+        sub        rcx, 50
+        call       encode_byte
+        or         qword [upper], rax
         ret
+.lower:
+        call       encode_byte
+        or         qword [lower], rax
+        ret
+
+encode_byte:
+        mov        rax, 1
+.l1:
+        cmp        rcx, 8
+        jb         .d1
+        shl        rax, 8
+        sub        rcx, 8
+        jmp        .l1
+.d1:
+        shl        rax, cl
+        ret
+
 
 ; @param   {address} rsi - input text
 ; @param   {address} rdi - output data
@@ -73,7 +108,8 @@ segment readable writable
 
 ; Data to hold scratch card information
 winners rb 10
-numbers rb 25
+lower rb 8
+upper rb 8
 
 input file "input-4"
 input_len = $ - input
