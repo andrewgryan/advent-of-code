d7014cca09998098698acedbe3383e96074974da
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-01-09 23:08:16 +0000

    first attempt

--- 2023/x86-64/day-5.asm
+++ 2023/x86-64/day-5.asm
@@ -13,16 +13,56 @@ main:
 
 
 seed_to_soil:
-        mov         rsi, 98
-        mov         rdx, 2
+        push        rbp
+        mov         rbp, rsp
+        sub         rsp, 8
+        mov         qword [rbp - 8], rdi ; Number
 
-        mov         rsi, 50
-        mov         rdx, 48
+        mov         rdi, qword [rbp - 8] ; Number
+        mov         rsi, 50              ; Source range start
+        mov         rdx, 48              ; Range length
+        call        in_range
 
-        mov         rax, 81
+        mov         rdi, qword [rbp - 8] ; Number
+        mov         rsi, 50              ; Source range start
+        mov         rdx, 52              ; Destination range start
+        call        apply_range
+
+        mov         rsp, rbp
+        pop         rbp
         ret
 
 
+; Range start <= N < range start + length
+;
+; @param {int} rdi - number
+; @param {int} rsi - source range start
+; @param {int} rdx - range length
+in_range:
+        ;          range start <= N
+        xor        rax, rax
+        cmp        rdi, rsi
+        setae      al
+
+        ;          Number < range start + length
+        xor        rdx, rdx
+        sub        rdi, rsi
+        cmp        rdi, rdx
+        setb       dl
+
+        ;          Range start <= N < range start + length
+        and        al, dl
+        ret
+
+
+; @param {int} rdi - number
+; @param {int} rsi - source range start
+; @param {int} rdx - destination range start
+apply_range:
+        mov        rax, rdi
+        sub        rax, rsi
+        add        rax, rdx
+        ret
 
 
 segment readable writable
