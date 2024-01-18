073ce0cae7b99501290c2950d4ef0d1d9b0feb7e
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-01-18 09:03:04 +0000

    .

--- 2023/x86-64/day-5.asm
+++ 2023/x86-64/day-5.asm
@@ -445,34 +445,17 @@ apply_range:
 
 segment readable writable
 
-input file "input-5"
+input file "example-5"  ; change to input-5 to solve puzzle
 input_len = $ - input
 
 seed_to_soil_label db "seed-to-soil map:", 0xA
 seed_to_soil_label_len = $ - seed_to_soil_label
-seed_to_soil_memory rq MAX_MAP_SIZE
 
 ; destination range start, source range start, range length
-seed_to_soil_map dq 2, 50, 98, 2, 52, 50, 48
-soil_to_fertilizer_map dq 3, \
-        0, 15, 37, \
-        37, 52, 2, \
-        39, 0, 15
-fertilizer_to_water_map dq 4, \
-        49, 53, 8, \
-        0, 11, 42, \
-        42, 0, 7, \
-        57, 7, 4 
-water_to_light_map dq 2, \
-        88, 18, 7, \
-        18, 25, 70
-light_to_temperature_map dq 3, \
-        45, 77, 23, \
-        81, 45, 19, \
-        68, 64, 13
-temperature_to_humidity_map dq 2, \
-        0, 69, 1, \
-        1, 0, 69
-humidity_to_location_map dq 2, \
-        60, 56, 37, \
-        56, 93, 4
+seed_to_soil_map rq MAX_MAP_SIZE
+soil_to_fertilizer_map rq MAX_MAP_SIZE
+fertilizer_to_water_map rq MAX_MAP_SIZE
+water_to_light_map rq MAX_MAP_SIZE
+light_to_temperature_map rq MAX_MAP_SIZE
+temperature_to_humidity_map rq MAX_MAP_SIZE
+humidity_to_location_map rq MAX_MAP_SIZE
