8350492df29a6aa8c2a62cdd21bb79c434da1f91
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-01-20 19:07:37 +0000

    .

--- 2023/x86-64/day-7.asm
+++ 2023/x86-64/day-7.asm
@@ -7,22 +7,132 @@ include "util.inc"
 segment readable executable
 entry main
 main:
-        exit 0
+        mov        rdi, five_of_a_kind
+        call       is_five_of_a_kind
+        mov        rdi, full_house
+        call       is_five_of_a_kind
+        int3
+        exit       0
 
 
-five_of_kind:
-        movzx r8, 1
-        mov sil, byte [rdi]
+hand:
+        call       is_five_of_a_kind
+        cmp        rax, 1
+        je         .five_of_a_kind
 
-        cmp sil, byte [rdi + 1]
-        sete al
-        imul r8b, al
+        call       is_four_of_a_kind
+        cmp        rax, 1
+        je         .four_of_a_kind
 
-        cmp sil, byte [rdi + 2]
-        cmp sil, byte [rdi + 3]
-        cmp sil, byte [rdi + 4]
+        call       is_full_house
+        cmp        rax, 1
+        je         .full_house
+
+        call       is_three_of_a_kind
+        cmp        rax, 1
+        je         .three_of_a_kind
+
+        call       is_two_pair
+        cmp        rax, 1
+        je         .two_pair
+
+        call       is_one_pair
+        cmp        rax, 1
+        je         .one_pair
+
+        call       is_high_card
+        cmp        rax, 1
+        je         .high_card
+
+.five_of_a_kind:
+        mov        rax, 6
+        ret
+
+.four_of_a_kind:
+        mov        rax, 5
+        ret
+
+.full_house:
+        mov        rax, 4
+        ret
+
+.three_of_a_kind:
+        mov        rax, 3
+        ret
+
+.two_pair:
+        mov        rax, 2
+        ret
+
+.one_pair:
+        mov        rax, 1
+        ret
+
+.high_card:
+        mov        rax, 0
+        ret
+
+
+; @param {*Hand} - Address of hand
+is_five_of_a_kind:
+        movzx      al, 1
+        movzx      r8, byte [rdi]
+
+        cmp        r8b, byte [rdi + 1]
+        sete       r9b
+        imul       al, r9b
+
+        cmp        r8b, byte [rdi + 2]
+        sete       r9b
+        imul       al, r9b
+
+        cmp        r8b, byte [rdi + 3]
+        sete       r9b
+        imul       al, r9b
+
+        cmp        r8b, byte [rdi + 4]
+        sete       r9b
+        imul       al, r9b
+        ret
+
+
+is_four_of_a_kind:
+        mov        rax, 1
+        ret
+
+
+is_full_house:
+        mov        rax, 1
+        ret
+
+
+is_three_of_a_kind:
+        mov        rax, 1
+        ret
+
+
+is_two_pair:
+        mov        rax, 1
+        ret
+
+
+is_one_pair:
+        mov        rax, 1
+        ret
+
+
+is_high_card:
+        mov        rax, 1
         ret
 
 
 segment readable writable
-order db "AKQJT98765432"
+order db "23456789TJQKA"
+
+five_of_a_kind db "AAAAA"
+four_of_a_kind db "AA8AA"
+full_house db "23332"
+three_of_a_kind db "TTT98"
+two_pair db "23432"
+one_pair db "A23A4"
+high_card db "23456"
