da1a03c384184f94927bf4a57a7b6fccee8193c2
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-01-20 22:06:54 +0000

    .

--- 2023/x86-64/day-7.asm
+++ 2023/x86-64/day-7.asm
@@ -7,10 +7,9 @@ include "util.inc"
 segment readable executable
 entry main
 main:
-        mov        rdi, five_of_a_kind
-        call       is_five_of_a_kind
         mov        rdi, full_house
-        call       is_five_of_a_kind
+        mov        rsi, ranks
+        call       count_ranks
         int3
         exit       0
 
@@ -98,6 +97,8 @@ is_five_of_a_kind:
 
 
 is_four_of_a_kind:
+        ; How to count 4 unique values?
+
         mov        rax, 1
         ret
 
@@ -127,8 +128,60 @@ is_high_card:
         ret
 
 
+; @param {int[]} rdi - Hand
+; @param {int[]} rsi - Ranks
+count_ranks:
+        ;          Zero array of ranks
+        xor        rcx, rcx
+        jmp        .l2
+.l1:
+        mov        byte [rsi + rcx], 0
+        inc        rcx
+.l2:
+        cmp        rcx, 5
+        jb         .l1
+
+        ;          Count ranks
+        xor        rcx, rcx
+        jmp        .l4
+.l3:
+        ;          Hash ASCII character
+        push       rcx
+        push       rdi
+        movzx      rdi, byte [rdi + rcx]
+        call       hash
+        pop        rdi
+        pop        rcx
+
+        ;          Increment count and index
+        inc        byte [rsi + rax]
+        inc        rcx
+.l4:
+        cmp        rcx, 5
+        jb         .l3
+        ret
+
+
+; Map from card rank to array index
+;
+; @param {int} - ASCII character
+; @returns {int}
+hash:
+        xor        rcx, rcx
+.l1:
+        cmp        dil, byte[order + rcx]
+        je         .l2
+
+        inc        rcx
+        jmp        .l1
+.l2:
+        mov        rax, rcx
+        ret
+
+
 segment readable writable
 order db "23456789TJQKA"
+ranks rb 13
 
 five_of_a_kind db "AAAAA"
 four_of_a_kind db "AA8AA"
