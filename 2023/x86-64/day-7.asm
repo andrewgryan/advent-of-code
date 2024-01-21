36c08bf2e8a66c5d3682f9637133f03332dbe06a
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-01-21 18:58:28 +0000

    .

--- 2023/x86-64/day-7.asm
+++ 2023/x86-64/day-7.asm
@@ -3,45 +3,61 @@ format ELF64 executable
 
 include "util.inc"
 
+CARDS_IN_HAND = 5
 NUMBER_OF_RANKS = 13
+NUMBER_OF_COMBOS = 6        ; None, Single, Pair, 3, 4, 5
 
 
 segment readable executable
 entry main
 main:
-        mov        rdi, full_house
-        call       is_full_house
         mov        rdi, three_of_a_kind
-        call       is_full_house
+        call       hand
         int3
         exit       0
 
 
+; @param {int[]} rdi - Hand
 hand:
+        mov        rsi, ranks
+        call       count_ranks
+
+        mov        rdi, ranks
+        mov        rsi, combos
+        call       count_combos
+
+        ;          Analyse combinations
+        mov        rdi, combos
         call       is_five_of_a_kind
         cmp        rax, 1
         je         .five_of_a_kind
 
+        mov        rdi, combos
         call       is_four_of_a_kind
         cmp        rax, 1
         je         .four_of_a_kind
 
+        mov        rdi, combos
         call       is_full_house
         cmp        rax, 1
         je         .full_house
 
+        mov        rdi, combos
         call       is_three_of_a_kind
         cmp        rax, 1
         je         .three_of_a_kind
 
+        mov        rdi, combos
         call       is_two_pair
         cmp        rax, 1
         je         .two_pair
 
+        mov        rdi, combos
         call       is_one_pair
         cmp        rax, 1
         je         .one_pair
 
+        mov        rdi, combos
         call       is_high_card
         cmp        rax, 1
         je         .high_card
@@ -75,105 +91,104 @@ hand:
         ret
 
 
-; @param {*Hand} - Address of hand
+; @param {int[]} rdi - Combos
 is_five_of_a_kind:
-        xor        r9, r9
-        mov        rax, 1
-        movzx      r8, byte [rdi]
-
-        cmp        r8b, byte [rdi + 1]
-        sete       r9b
-        imul       rax, r9
-
-        cmp        r8b, byte [rdi + 2]
-        sete       r9b
-        imul       rax, r9
-
-        cmp        r8b, byte [rdi + 3]
-        sete       r9b
-        imul       rax, r9
-
-        cmp        r8b, byte [rdi + 4]
-        sete       r9b
-        imul       rax, r9
+        xor        rax, rax
+        cmp        byte [rdi + 5], 1
+        sete       al
         ret
 
 
-; @param {int[]} rdi - Hand
+; @param {int[]} rdi - Combos
 is_four_of_a_kind:
-        mov         rsi, 4
-        call        of_a_kind
+        xor        rax, rax
+        cmp        byte [rdi + 4], 1
+        sete       al
         ret
 
 
-; @param {int[]} rdi - Hand
-; @param {int}   rsi - Count
-of_a_kind:
-        push       rsi
-        mov        rsi, ranks
-        call       count_ranks
-        pop        rsi
+; @param {int[]} rdi - Combos
+is_full_house:
+        xor        rax, rax
+        cmp        byte [rdi + 3], 1
+        sete       r8b
+        cmp        byte [rdi + 2], 1
+        sete       al
+        and        al, r8b
+        ret
 
-        ;          Detect a 4
+
+; @param {int[]} rdi - Combos
+is_three_of_a_kind:
+        xor        rax, rax
+        cmp        byte [rdi + 3], 1
+        sete       r8b
+        cmp        byte [rdi + 1], 2
+        sete       al
+        and        al, r8b
+        ret
+
+
+; @param {int[]} rdi - Combos
+is_two_pair:
+        xor        rax, rax
+        cmp        byte [rdi + 2], 2
+        sete       al
+        ret
+
+
+; @param {int[]} rdi - Combos
+is_one_pair:
+        xor        rax, rax
+        cmp        byte [rdi + 2], 1
+        sete       r8b
+        cmp        byte [rdi + 1], 3
+        sete       al
+        and        al, r8b
+        ret
+
+
+; @param {int[]} rdi - Combos
+is_high_card:
+        xor        rax, rax
+        cmp        byte [rdi + 1], 5
+        sete       al
+        ret
+
+
+; Count occurences of ones, pairs, three, four, five of a kind
+;
+; @param {int[]} rdi - Ranks
+; @param {int[]} rsi - Combos
+count_combos:
+        ;          Zero array of combinations
         xor        rcx, rcx
         jmp        .l2
 .l1:
-        cmp        byte [ranks + rcx], sil
-        je         .found
-
+        mov        byte [rsi + rcx], 0
         inc        rcx
 .l2:
-        cmp        rcx, NUMBER_OF_RANKS
+        cmp        rcx, NUMBER_OF_COMBOS
         jb         .l1
-        mov        rax, 0
-        ret
 
-.found:
-        mov        rax, 1
-        ret
+        ;          Count combinations
+        xor        rcx, rcx
+        jmp        .l4
+.l3:
+        ;          Read kind of combination
+        movzx      r8, byte [rdi + rcx]
 
-
-is_full_house:
-        push        rdi
-        mov         rsi, 3
-        call        of_a_kind
-        pop         rdi
-        push        rax
-
-        push        rdi
-        mov         rsi, 2
-        call        of_a_kind
-        pop         rdi
-        push        rax
-
-        pop         rax
-        pop         r8
-        imul        rax, r8
+        ;          Increment count and index
+        inc        byte [rsi + r8]
+        inc        rcx
+.l4:
+        cmp        rcx, NUMBER_OF_RANKS
+        jb         .l3
         ret
 
 
-is_three_of_a_kind:
-        mov         rsi, 3
-        call        of_a_kind
-        ret
-
-
-is_two_pair:
-        mov        rax, 1
-        ret
-
-
-is_one_pair:
-        mov         rsi, 2
-        call        of_a_kind
-        ret
-
-
-is_high_card:
-        mov        rax, 1
-        ret
-
-
+; Count occurences of 2, 3, 4, ..., T, J, K, Q, A
+;
 ; @param {int[]} rdi - Hand
 ; @param {int[]} rsi - Ranks
 count_ranks:
@@ -184,7 +199,7 @@ count_ranks:
         mov        byte [rsi + rcx], 0
         inc        rcx
 .l2:
-        cmp        rcx, 5
+        cmp        rcx, NUMBER_OF_RANKS
         jb         .l1
 
         ;          Count ranks
@@ -203,7 +218,7 @@ count_ranks:
         inc        byte [rsi + rax]
         inc        rcx
 .l4:
-        cmp        rcx, 5
+        cmp        rcx, CARDS_IN_HAND
         jb         .l3
         ret
 
@@ -228,6 +243,7 @@ hash:
 segment readable writable
 order db "23456789TJQKA"
 ranks rb NUMBER_OF_RANKS
+combos rb NUMBER_OF_COMBOS
 
 five_of_a_kind db "AAAAA"
 four_of_a_kind db "AA8AA"
