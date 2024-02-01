d696fe653cefef98ad3c850149427063e498e4a9
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-02-01 21:53:20 +0000

    map character sequence to number

--- 2023/x86-64/day-8.asm
+++ 2023/x86-64/day-8.asm
@@ -7,6 +7,33 @@ include "exit.asm"
 segment readable executable
 entry main
 main:
-        exit 0
+        mov        rdi, node
+        call       hash
+        int3
+        exit       0
+
+
+; @param rdi Address of node
+hash:
+        xor        rax, rax
+        xor        rcx, rcx
+        mov        r9, 2
+        mov        r10, 1
+        jmp        .l1
+.l2:
+        movzx      r11, byte [rdi + r9]   ; Read character
+        sub        r11, 'A'               ; Convert to number
+        imul       r11, r10               ; Multiply by base
+        add        rax, r11               ; Sum accumulator
+        inc        rcx                    ; Increase counter
+        dec        r9
+        imul       r10, 26                ; Next power of 26
+.l1:
+        cmp        rcx, 3
+        jb         .l2
+        ret
+
 
 segment readable writable
+network rw 26 * 26 * 26
+node db "ABC"
