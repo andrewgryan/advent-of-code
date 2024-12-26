e49811260fba07cf72716a60e47f06e478c1fae1
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-12-26 20:00:59 +0000

    experiment with array of pointers

--- 2024/gas/src/main.asm
+++ 2024/gas/src/main.asm
@@ -1,6 +1,18 @@
+.data
+	pointer: .space 1024, 0
+
 .text
 .global _start
 _start:
+	mov	$pointer, %rdx
+	movb	$60, 0x8(%rdx)
+	lea	0x8(%rdx), %rcx
+	mov	%rcx, 0x0(%rdx)
+
 	mov	$60, %rax
 	mov	$1, %rdi
 	syscall
+
+
+sum:
+	ret
