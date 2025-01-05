bc5eb9327d0eba0162141a0c2977d887a2c50017
andrewgryan <andrew.ryan@metoffice.gov.uk>
2025-01-05 16:47:46 +0000

    hello, world aarch64

--- 2024/aarch64/hello.asm
+++ 2024/aarch64/hello.asm
@@ -1,6 +1,17 @@
+.data
+msg:
+        .ascii "Hello, World!\n"
+msg_len = . - msg
+
 .text
 .global _start
 _start:
+        mov x0, #1
+        ldr x1, =msg
+        ldr x2, =msg_len
+        mov w8, #64
+        svc #0
+
         mov w8, #93
         mov x0, #0
         svc #0
