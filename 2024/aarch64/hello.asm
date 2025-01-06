cde1f95b4dbaff9eccdbed4d5734b51f87b7627a
andrewgryan <andrew.ryan@metoffice.gov.uk>
2025-01-06 22:45:12 +0000

    wip

--- 2024/aarch64/hello.asm
+++ 2024/aarch64/hello.asm
@@ -1,7 +1,18 @@
 .data
+O_RDONLY = 0
+O_WRONLY = 1
+O_CREAT = 100 // Create and write is 101
+
+SYS_WRITE = 64
+SYS_OPENAT = 56
+SYS_EXIT = 93
+
+AT_FDCWD = -100
 msg:
         .ascii "Hello, World!\n"
 msg_len = . - msg
+fname:
+        .asciz "file.txt"
 
 .text
 .global _start
@@ -9,9 +20,21 @@ _start:
         mov x0, #1
         ldr x1, =msg
         ldr x2, =msg_len
-        mov w8, #64
+        ldr w8, =SYS_WRITE
         svc #0
 
-        mov w8, #93
-        mov x0, #0
+        ldr w8, =SYS_OPENAT
+        ldr x0, =AT_FDCWD
+        ldr x1, =fname
+        mov x2, #101
+        mov x3, #400
+        svc #0
+
+        ldr x1, =msg
+        ldr x2, =msg_len
+        ldr w8, =SYS_WRITE
+        svc #0
+
+        ldr w8, =SYS_EXIT
+        # mov x0, #0
         svc #0
