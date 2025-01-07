8406230ae4a006ea6aacdf88f7b5652659983ecb
andrewgryan <andrew.ryan@metoffice.gov.uk>
2025-01-07 10:31:15 +0000

    mmap allocate memory

--- 2024/aarch64/hello.asm
+++ 2024/aarch64/hello.asm
@@ -9,6 +9,16 @@ SYS_EXIT = 93
 SYS_MMAP = 222
 
 AT_FDCWD = -100
+
+PROT_NONE = 0x0
+PROT_READ = 0x1
+PROT_WRITE = 0x2
+PROT_EXEC = 0x4
+
+MAP_SHARED = 0x01
+MAP_PRIVATE = 0x02
+MAP_ANONYMOUS = 0x20
+
 msg:
         .ascii "Hello, World!\n"
 msg_len = . - msg
@@ -34,12 +44,22 @@ _start:
         mov x0, x4
         mov x0, #0x0
         mov x1, #14
-        mov x2, #0x1
-        mov x3, #0x2
+        ldr x2, =PROT_READ
+        ldr x3, =MAP_PRIVATE
         mov x5, #0x0
         ldr w8, =SYS_MMAP
         svc #0
 
+        mov x0, #0x0
+        mov x1, #1024
+        ldr x2, =(PROT_READ | PROT_WRITE)
+        ldr x3, =(MAP_PRIVATE | MAP_ANONYMOUS)
+        mov x4, #-1
+        ldr w8, =SYS_MMAP
+        svc #0
+
+        mov x9, #42
+        strb w9, [x0]
         ldrb w0, [x0]
 
         ldr w8, =SYS_EXIT
