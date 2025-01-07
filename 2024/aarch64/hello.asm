bf58539a45da30d4c025a5e5d5496cf74380b38e
andrewgryan <andrew.ryan@metoffice.gov.uk>
2025-01-07 09:37:06 +0000

    use mmap to load file

--- 2024/aarch64/hello.asm
+++ 2024/aarch64/hello.asm
@@ -6,6 +6,7 @@ O_CREAT = 100 // Create and write is 101
 SYS_WRITE = 64
 SYS_OPENAT = 56
 SYS_EXIT = 93
+SYS_MMAP = 222
 
 AT_FDCWD = -100
 msg:
@@ -26,15 +27,20 @@ _start:
         ldr w8, =SYS_OPENAT
         ldr x0, =AT_FDCWD
         ldr x1, =fname
-        mov x2, #101
+        ldr x2, =O_RDONLY
         mov x3, #400
         svc #0
 
-        ldr x1, =msg
-        ldr x2, =msg_len
-        ldr w8, =SYS_WRITE
+        mov x0, x4
+        mov x0, #0x0
+        mov x1, #14
+        mov x2, #0x1
+        mov x3, #0x2
+        mov x5, #0x0
+        ldr w8, =SYS_MMAP
         svc #0
 
+        ldrb w0, [x0]
+
         ldr w8, =SYS_EXIT
-        # mov x0, #0
         svc #0
