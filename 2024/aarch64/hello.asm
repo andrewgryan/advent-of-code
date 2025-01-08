f766248745ddbee90b788ed375c6b9eef2b0c0b6
andrewgryan <andrew.ryan@metoffice.gov.uk>
2025-01-08 19:56:05 +0000

    mmap file and print

--- 2024/aarch64/hello.asm
+++ 2024/aarch64/hello.asm
@@ -27,6 +27,7 @@ KB = 1024
 
 STDOUT = 0x1
 
+
 msg:
         .ascii "Hello, World!\n"
 msg_len = . - msg
@@ -39,32 +40,29 @@ fd: .dword 0
 .text
 .global _start
 
-
-stat:
-        ldr x0, =AT_FDCWD
-        ldr x1, =fname
-        ldr x2, =stat_buf
-        mov x3, #0x0
-        ldr w8, =SYS_FSTATAT
-        svc #0
-
-        ldr x0, [x2, #48]
-        ret
-
 main:
         stp x29, x30, [sp, #16]
 
+        ldr x0, =fname
+        bl stat
+
+
+        ldr x0, =fname
         bl open_file
+
+        ldr x2, =stat_buf
+        ldr x1, [x2, #48]
         bl mmap_file
 
-        mov x1, x0
-        mov x2, #15
+        ldr x2, =stat_buf
+        ldr x2, [x2, #48]
         bl write
 
         ldp x29, x30, [sp, #16]
         ret
 
 write:
+        mov x1, x0
         ldr x0, =STDOUT
         ldr w8, =SYS_WRITE
         svc #0
@@ -72,19 +70,31 @@ write:
 
 
 open_file:
+        mov x1, x0
         ldr w8, =SYS_OPENAT
         ldr x0, =AT_FDCWD
-        ldr x1, =fname
         ldr x2, =O_RDONLY
         mov x3, #400
         svc #0
         ret
 
 
+stat:
+        mov x1, x0
+        ldr x0, =AT_FDCWD
+        ldr x2, =stat_buf
+        mov x3, #0x0
+        ldr w8, =SYS_FSTATAT
+        svc #0
+        ret
+
+/**
+ * x0 - file descriptor
+ * x1 - length
+ */
 mmap_file:
         mov x4, x0
         ldr x0, =NULL
-        mov x1, #14
         ldr x2, =PROT_READ
         ldr x3, =MAP_PRIVATE
         mov x5, #0x0
