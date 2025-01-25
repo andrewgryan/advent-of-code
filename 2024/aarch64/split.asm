2dd9b398ce506ccf1b1b39d81ddff70e204eabec
andrewgryan <andrew.ryan@metoffice.gov.uk>
2025-01-25 22:56:31 +0000

    atoi

--- 2024/aarch64/split.asm
+++ 2024/aarch64/split.asm
@@ -2,7 +2,7 @@
 SYS_EXIT = 93
 
 txt:
-        .ascii "65"
+        .ascii "127"
 txt_len = . - txt
 
 .text
@@ -13,7 +13,7 @@ main:
         mov fp, sp
 
         ldr x0, =txt
-        mov x1, #2
+        ldr x1, =txt_len
         bl atoi
 
         mov sp, fp
@@ -25,23 +25,22 @@ atoi:
         mov fp, sp
 
 
-        mov x3, x1
+        mov x3, #0
         mov x4, x0
         mov x5, #10
         mov x6, #0
 
         b 2f
 1:
-        sub x3, x3, #1
-
         ldrb w0, [x4, x3]
         bl ctoi
 
         mul x6, x6, x5
         add x6, x6, x0
+        add x3, x3, #1
 2:
-        cmp x3, #0
-        b.ne 1b
+        cmp x3, x1
+        b.lt 1b
 
         mov x0, x6
 
